eb6727febbdbe229731c0f9793bb3ca9a5658f43
Pedro Arns Junior <pedroarnsjr@outlook.com>
2023-06-21 22:26:07 -0300

    graphql-filters

--- capputeeno/src/utils/graphql-filters.ts
+++ capputeeno/src/utils/graphql-filters.ts
@@ -8,18 +8,15 @@ export function getCategoryByType(type: FilterType){
 }
 
 export function getFieldByPriority(priority: PriorityTypes){
-   if(priority === PriorityTypes.NEWS) 
-      return {field: "created_at", order: "ASC"}
-   if(priority === PriorityTypes.BIGGEST_PRICE)
-      return {field: "price_in_cents", order: "ASC"}
-   if(priority == PriorityTypes.MINOR_PRICE)
-      return {field: "price_in_cents", order: "ASC"}
-      return {field: "sales", order: "DSC"}
+    if(priority === PriorityTypes.NEWS) return {field: "created_at", order: "ASC"}
+    if(priority === PriorityTypes.BIGGEST_PRICE)  return {field: "price_in_cents", order: "ASC"}
+    if(priority === PriorityTypes.MINOR_PRICE) return {field: "price_in_cents", order: "ASC"}
+    return {field: "sales", order: "DSC"}
 }
 
-const mountQuery = (type: FilterType, priority: PriorityTypes) => {
-    if(type === FilterType.ALL && priority == PriorityTypes.POPULARITY) return `query{
-        allProducts(sortField: "sales", sortOrder: "DSC"){
+export const mountQuery = (type: FilterType, priority: PriorityTypes) => {
+    if(type === FilterType.ALL && priority === PriorityTypes.POPULARITY) return `query {
+        allProducts(sortField: "sales", sortOrder: "DSC") {
           id
           name
           price_in_cents
@@ -30,14 +27,14 @@ const mountQuery = (type: FilterType, priority: PriorityTypes) => {
     const sortSettings = getFieldByPriority(priority)
     const categoryFilter = getCategoryByType(type)
     return `
-     query{
-         allProducts(sortField: "${sortSettings.field}", sortOrder: "${sortSettings.order}", ${categoryFilter ? `filter: {category: "${categoryFilter}"`: ''}){
-           id
-           name
-           price_in_cents
-           image_url
-           category
-         }
-       }
+    query {
+        allProducts(sortField: "${sortSettings.field}", sortOrder: "${sortSettings.order}", ${categoryFilter ? `filter: { category: "${categoryFilter}"}`: ''}) {
+          id
+          name
+          price_in_cents
+          image_url
+          category
+        }
+      }
     `
-  }
+}
